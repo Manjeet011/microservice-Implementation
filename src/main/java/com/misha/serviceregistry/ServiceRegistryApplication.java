4bb6dda82aef0be07235df5209c0534742e04dd5
Manjeet011 <manjeet841401@gmail.com>
2025-05-07 14:50:06 +0530

    Push registry

---
+++ src/main/java/com/misha/serviceregistry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.misha.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+}
